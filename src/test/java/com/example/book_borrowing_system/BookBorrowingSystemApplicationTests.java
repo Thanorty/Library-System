c27128be4e9c1f98ad4b683adff0b64e03f3f428
Koghu <koghulan.rajamanickam@azeusconvene.com>
2025-01-07 02:15:31 +0800

    Added Test Containers

--- src/test/java/com/example/book_borrowing_system/BookBorrowingSystemApplicationTests.java
+++ src/test/java/com/example/book_borrowing_system/BookBorrowingSystemApplicationTests.java
@@ -2,12 +2,23 @@ package com.example.book_borrowing_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class BookBorrowingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+			.withDatabaseName("library_db")
+			.withUsername("library_user")
+			.withPassword("library_password");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
 	}
 
 }
